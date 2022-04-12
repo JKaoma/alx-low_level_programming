f007516efabe86a1faaa8682794bd6bfc1c52921
JKaoma <james.kaoma@outlook.com>
2022-04-12 14:05:02 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
  * main - Entry point
- * 
+ * Description: print _putchar in a new line
  * Return: this program returns zero
  */
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
@@ -16,6 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
