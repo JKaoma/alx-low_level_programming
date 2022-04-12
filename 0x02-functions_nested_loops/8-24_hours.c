057ad2d9b8d7ddc27d955f5e3d72b97ea1656cd1
JKaoma <james.kaoma@outlook.com>
2022-04-12 15:35:30 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /** 
  * jack_bauer - prints every minute of the day
  * 
@@ -8,7 +7,6 @@
 void jack_bauer(void)
 {
 int hour, minute;
-
 for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
